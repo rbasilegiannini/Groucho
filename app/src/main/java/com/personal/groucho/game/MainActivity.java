e9ecca12e9713642d107f430cec533f83c8c3e28
rbgdotbrush <rbg94@hotmail.it>
2024-01-06 19:52:58 +0100

    Override onCreate, onPause, onStop and onResume in MainActivity

--- app/src/main/java/com/personal/groucho/game/MainActivity.java
+++ app/src/main/java/com/personal/groucho/game/MainActivity.java
@@ -1,6 +1,56 @@
 package com.personal.groucho.game;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.personal.groucho.R;
 
 public class MainActivity extends Activity {
+
+    public static String TAG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        System.loadLibrary("liquidfun");
+        System.loadLibrary("liquidfun_jni");
+
+        TAG = getString(R.string.app_name);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // Game world
+
+        // View
+
+        // Touch
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.i("Main thread", "pause");
+
+        // Other
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i("Main thread", "stop");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.i("Main thread", "resume");
+
+        // Other
+    }
 }
