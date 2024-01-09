1bdf623047f5d1c03880b25830052f3de3c124c7
rbgdotbrush <rbg94@hotmail.it>
2024-01-09 02:58:43 +0100

    Add custom AndroidFastRenderView

---
+++ app/src/main/java/com/personal/groucho/game/AndroidFastRenderView.java
@@ -0,0 +1,59 @@
+package com.personal.groucho.game;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class AndroidFastRenderView extends SurfaceView implements Runnable {
+    private final GameWorld gameWorld;
+
+    Bitmap framebuffer;
+    Thread renderThread = null;
+    SurfaceHolder holder;
+    volatile boolean running = false;
+    public AndroidFastRenderView(Context context, GameWorld gameWorld) {
+        super(context);
+        this.gameWorld = gameWorld;
+        this.framebuffer = gameWorld.buffer;
+        this.holder = getHolder();
+    }
+    public void resume() {
+        running = true;
+        renderThread = new Thread(this);
+        renderThread.start();
+    }
+    public void run() {
+        Rect dstRect = new Rect();
+        long startTime = System.nanoTime();
+        while(running) {
+            if(!holder.getSurface().isValid())
+                continue;
+
+            float deltaTime = (System.nanoTime()-startTime) / 1000000000.0f;
+            startTime = System.nanoTime();
+
+            gameWorld.update(deltaTime);
+            gameWorld.render();
+
+            Canvas canvas = holder.lockCanvas();
+            canvas.getClipBounds(dstRect);
+            canvas.drawBitmap(framebuffer, null, dstRect, null);
+            holder.unlockCanvasAndPost(canvas);
+        }
+    }
+    public void pause() {
+        running = false;
+        while(true) {
+            try {
+                renderThread.join();
+                return;
+            } catch (InterruptedException e) {
+                // retry
+            }
+        }
+    }
+}
+
