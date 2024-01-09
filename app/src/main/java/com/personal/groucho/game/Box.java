15ea6193488c67c59966f9c37d1fcae8921e2f5d
rbgdotbrush <rbg94@hotmail.it>
2024-01-10 00:57:31 +0100

    Add Box Class

---
+++ app/src/main/java/com/personal/groucho/game/Box.java
@@ -0,0 +1,14 @@
+package com.personal.groucho.game;
+
+public class Box {
+    public float xmin, ymin, xmax, ymax, width, height;
+    public Box(float xmin, float ymin, float xmax, float ymax)
+    {
+        this.xmin = xmin;
+        this.xmax = xmax;
+        this.ymin = ymin;
+        this.ymax = ymax;
+        this.width = xmax - xmin;
+        this.height = ymax - ymin;
+    }
+}
