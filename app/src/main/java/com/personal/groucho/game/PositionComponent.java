fa361df8e5ca7bc0e00becfe4e9f1a771ca0fe0f
rbgdotbrush <rbg94@hotmail.it>
2024-01-10 01:11:15 +0100

    Add PositionComponent

---
+++ app/src/main/java/com/personal/groucho/game/PositionComponent.java
@@ -0,0 +1,23 @@
+package com.personal.groucho.game;
+
+public class PositionComponent extends Component{
+    private int posX;
+    private int posY;
+
+    public PositionComponent(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    @Override
+    public ComponentType type() {
+        return ComponentType.Position;
+    }
+
+    public int getPosX() {return this.posX;}
+    public int getPosY() {return this.posY;}
+
+    public void setPosX(int newPosX) {this.posX = newPosX;}
+    public void setPosY(int newPosY) {this.posY = newPosY;}
+
+}
