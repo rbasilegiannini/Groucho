92539fa8d466a55b339ef259a1be19093858a217
rbgdotbrush <rbg94@hotmail.it>
2024-01-11 22:26:33 +0100

    Add AnimationType constants

--- app/src/main/java/com/personal/groucho/game/AnimationType.java
+++ app/src/main/java/com/personal/groucho/game/AnimationType.java
@@ -1,8 +1,8 @@
 package com.personal.groucho.game;
 
 public class AnimationType {
-    public static int UP = 0;
-    public static int DOWN = 2;
-    public static int RIGHT = 3;
-    public static int LEFT = 1;
+    public static final int UP = 0;
+    public static final int DOWN = 2;
+    public static final int RIGHT = 3;
+    public static final int LEFT = 1;
 }
