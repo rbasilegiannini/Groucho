3d49b9764eed6c74e260075fe1fff0e3cf43eb25
rbgdotbrush <rbg94@hotmail.it>
2024-01-11 18:37:03 +0100

    [WIP] Implement drawAnimation method in Spritesheet Class

--- app/src/main/java/com/personal/groucho/game/Spritesheet.java
+++ app/src/main/java/com/personal/groucho/game/Spritesheet.java
@@ -2,17 +2,18 @@ package com.personal.groucho.game;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 
 public class Spritesheet {
 
-    private Bitmap sheet;
+    private final Bitmap sheet;
 
     private int frameWidth;
     private int frameHeight;
 
-    private int[] start;
-    private int[] length;
-    private int[] delay;
+    private final int[] start;
+    private final int[] length;
+    private final int[] delay;
 
     public Spritesheet(Bitmap sheet, int numberOfAnimations) {
         this.sheet = sheet;
@@ -32,11 +33,40 @@ public class Spritesheet {
         this.delay[animation] = delay;
     }
 
-    public void drawFrame(Canvas canvas, int frame, int x, int y) {
-        //
+    public int drawAnimation(Canvas canvas, int animation, int step, int x, int y, int scaleFactor) {
+//        boolean isLoop = true;
+//        if (isLoop) {
+//            if (step >= length[animation] - 1)
+//                step = start[animation] + 1;
+//        }
+//        else {
+//            if (step >= length[animation] - 1)
+//                step = length[animation] - 1;
+//        }
+        if (step >= length[animation] - 1)
+            step = 1;
+
+        int srcTop = animation * frameHeight;
+        int srcLeft = step * frameWidth;
+        Rect src = new Rect(
+                srcLeft, srcTop,
+                srcLeft + frameWidth,
+                srcTop + frameHeight
+        );
+
+        int destTop = y-(scaleFactor*frameHeight/2);
+        int destLeft = x-(scaleFactor*frameWidth/2);
+        Rect dest = new Rect(
+                destLeft, destTop,
+                destLeft + scaleFactor*frameWidth - 1,
+                destTop + scaleFactor*frameHeight -1
+        );
+
+        canvas.drawBitmap(sheet, src, dest, null);
+        return step;
     }
 
-    public void drawAnimation(Canvas canvas, int animation, int step, int x, int y) {
-        //
+    public long getDelay(int animation) {
+        return delay[animation];
     }
 }
