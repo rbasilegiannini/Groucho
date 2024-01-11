e4426f845bbab4110d345434d5b79cca0709c3da
rbgdotbrush <rbg94@hotmail.it>
2024-01-11 18:31:41 +0100

    Implement consume touch events in Controller Class

--- app/src/main/java/com/personal/groucho/game/Controller.java
+++ app/src/main/java/com/personal/groucho/game/Controller.java
@@ -3,24 +3,32 @@ package com.personal.groucho.game;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
+
+import com.personal.groucho.badlogic.androidgames.framework.Input;
 
 public class Controller {
+    private final GameWorld gameWorld;
 
-    private float upDPadPosX, upDPadPosY;
-    private float downDPadPosX, downDPadPosY;
-    private float leftDPadPosX, leftDPadPosY;
-    private float rightDPadPosX, rightDPadPosY;
+    private final float upDPadPosX, upDPadPosY;
+    private final float downDPadPosX, downDPadPosY;
+    private final float leftDPadPosX, leftDPadPosY;
+    private final float rightDPadPosX, rightDPadPosY;
 
-    private int outerCircleRadius = 50;
-    private Paint innerCirclePaint;
-    private Paint outerCirclePaint;
+    boolean upPressed, downPressed, leftPressed, rightPressed;
 
-    public Controller(float controllerCenterX, float controllerCenterY) {
+    private final double radius, radiusSqr;
+
+    private final Paint circlePaint;
+
+    public Controller(float controllerCenterX, float controllerCenterY, GameWorld gw) {
+        gameWorld = gw;
+
         upDPadPosX = controllerCenterX;
-        upDPadPosY = controllerCenterY + 75;
+        upDPadPosY = controllerCenterY - 75;
 
         downDPadPosX = controllerCenterX;
-        downDPadPosY = controllerCenterY - 75;
+        downDPadPosY = controllerCenterY + 75;
 
         leftDPadPosX = controllerCenterX - 75;
         leftDPadPosY = controllerCenterY;
@@ -28,14 +36,12 @@ public class Controller {
         rightDPadPosX = controllerCenterX + 75;
         rightDPadPosY = controllerCenterY;
 
-        // paint of circles
-        outerCirclePaint = new Paint();
-        outerCirclePaint.setColor(Color.GRAY);
-        outerCirclePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        radius = 50;
+        radiusSqr = Math.pow(radius, 2);
 
-        innerCirclePaint = new Paint();
-        innerCirclePaint.setColor(Color.BLUE);
-        innerCirclePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        circlePaint = new Paint();
+        circlePaint.setColor(Color.GRAY);
+        circlePaint.setStyle(Paint.Style.FILL_AND_STROKE);
     }
 
     public void draw(Canvas canvas) {
@@ -43,36 +49,138 @@ public class Controller {
         canvas.drawCircle(
                 upDPadPosX,
                 upDPadPosY,
-                outerCircleRadius,
-                outerCirclePaint
+                (int)radius,
+                circlePaint
         );
 
         // Draw down D-Pad
         canvas.drawCircle(
                 downDPadPosX,
                 downDPadPosY,
-                outerCircleRadius,
-                outerCirclePaint
+                (int)radius,
+                circlePaint
         );
 
         // Draw left D-Pad
         canvas.drawCircle(
                 leftDPadPosX,
                 leftDPadPosY,
-                outerCircleRadius,
-                outerCirclePaint
+                (int)radius,
+                circlePaint
         );
 
         // Draw right D-Pad
         canvas.drawCircle(
                 rightDPadPosX,
                 rightDPadPosY,
-                outerCircleRadius,
-                outerCirclePaint
+                (int)radius,
+                circlePaint
         );
     }
 
-    public void update() {
+    public boolean isUpPressed() {return upPressed;}
+    public boolean isDownPressed() {return downPressed;}
+    public boolean isLeftPressed() {return leftPressed;}
+    public boolean isRightPressed() {return rightPressed;}
 
+    public void consumeTouchEvent(Input.TouchEvent event){
+        switch (event.type) {
+            case Input.TouchEvent.TOUCH_DOWN:
+                consumeTouchDown(event);
+                break;
+            case Input.TouchEvent.TOUCH_UP:
+                consumeTouchUp(event);
+                break;
+            case Input.TouchEvent.TOUCH_DRAGGED:
+                consumeTouchDragged(event);
+                break;
+        }
     }
+
+    private void consumeTouchDragged(Input.TouchEvent event) {
+        float x = gameWorld.fromScreenToBufferX(event.x);
+        float y = gameWorld.fromScreenToBufferY(event.y);
+
+        if (upPressed && !isInUp(x, y)) {upPressed = false;
+
+            Log.d("Controller", "released Up");}
+        if (downPressed && !isInDown(x, y)){
+            if(downPressed) {downPressed = false;
+
+                Log.d("Controller", "released Down");}
+        }
+        if (leftPressed && !isInLeft(x,y)) {
+            if(leftPressed) {leftPressed = false;
+
+                Log.d("Controller", "released Left");}
+        }
+        if (rightPressed && !isInRight(x,y)) {
+            if(rightPressed) {rightPressed = false;
+
+                Log.d("Controller", "released Right");}
+        }
+    }
+
+    private void consumeTouchUp(Input.TouchEvent event) {
+        float x = gameWorld.fromScreenToBufferX(event.x);
+        float y = gameWorld.fromScreenToBufferY(event.y);
+
+        if(isInUp(x, y)){
+            if(upPressed) {upPressed = false;
+
+                Log.d("Controller", "released Up");}
+        }
+        if (isInDown(x, y)){
+            if(downPressed) {downPressed = false;
+
+                Log.d("Controller", "released Down");}
+        }
+        if (isInLeft(x,y)) {
+            if(leftPressed) {leftPressed = false;
+
+                Log.d("Controller", "released Left");}
+        }
+        if (isInRight(x,y)) {
+            if(rightPressed) {rightPressed = false;
+
+                Log.d("Controller", "released Right");}
+        }
+    }
+
+    private void consumeTouchDown(Input.TouchEvent event) {
+        float x = gameWorld.fromScreenToBufferX(event.x);
+        float y = gameWorld.fromScreenToBufferY(event.y);
+
+        if(isInUp(x, y)){
+            if(!upPressed) {upPressed = true;
+
+            Log.d("Controller", "pressed Up");}
+        }
+        if (isInDown(x, y)){
+            if(!downPressed) {downPressed = true;
+
+            Log.d("Controller", "pressed Down");}
+        }
+        if (isInLeft(x,y)) {
+            if(!leftPressed) {leftPressed = true;
+
+            Log.d("Controller", "pressed Left");}
+        }
+        if (isInRight(x,y)) {
+            if(!rightPressed) {rightPressed = true;
+
+            Log.d("Controller", "pressed Right");}
+        }
+    }
+
+    private boolean isInRight(float x, float y) { return isInCircle(x, rightDPadPosX, y, rightDPadPosY); }
+    private boolean isInLeft(float x, float y) { return isInCircle(x, leftDPadPosX, y, leftDPadPosY); }
+    private boolean isInDown(float x, float y) { return isInCircle(x, downDPadPosX, y, downDPadPosY); }
+    private boolean isInUp(float x, float y) { return isInCircle(x, upDPadPosX, y, upDPadPosY); }
+
+    private boolean isInCircle(float x, float upDPadPosX, float y, float upDPadPosY) {
+        double pointerPositionSqr = Math.pow(x - upDPadPosX, 2) + Math.pow(y - upDPadPosY, 2);
+        return pointerPositionSqr <= radiusSqr;
+    }
+
 }
