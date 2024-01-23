52215737be3f050ec5bcd419b205a7e79acf2031
rbgdotbrush <rbg94@hotmail.it>
2024-01-23 20:10:12 +0100

    Update Constants

--- app/src/main/java/com/personal/groucho/game/Constants.java
+++ app/src/main/java/com/personal/groucho/game/Constants.java
@@ -4,4 +4,9 @@ public class Constants {
     public static int characterScaleFactor = 5;
     public static float speed = 0.2f;
 
+    public static int grouchoHealth = 100;
+    public static int skeletonHealth = 100;
+
+    public static int grouchoPower = 10;
+    public static int skeletonPower = 5;
 }
