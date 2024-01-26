5180f5562e6cfc012e54804f4668f3f0b9647f10
rbgdotbrush <rbg94@hotmail.it>
2024-01-26 20:35:35 +0100

    Add bullet hit sounds

--- app/src/main/java/com/personal/groucho/game/assets/Sounds.java
+++ app/src/main/java/com/personal/groucho/game/assets/Sounds.java
@@ -3,14 +3,18 @@ package com.personal.groucho.game.assets;
 import com.personal.groucho.badlogic.androidgames.framework.Audio;
 import com.personal.groucho.badlogic.androidgames.framework.Sound;
 
-public class PlayerSounds {
+public class Sounds {
     public static Sound loadingSound;
     public static Sound shootingSound;
     public static Sound click;
+    public static Sound bulletHitFurniture;
+    public static Sound bulletHitEnemy;
 
     public static void init(Audio audio) {
         loadingSound = audio.newSound("loading.mp3");
         shootingSound = audio.newSound("shot.mp3");
         click = audio.newSound("click.mp3");
+        bulletHitEnemy = audio.newSound("bullet_hit_enemy.mp3");
+        bulletHitFurniture = audio.newSound("bullet_hit_furniture.mp3");
     }
 }
