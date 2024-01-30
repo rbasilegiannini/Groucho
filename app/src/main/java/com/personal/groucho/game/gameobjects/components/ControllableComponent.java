95aeb74bed7a85304558d63c18b61474e0ef6e99
rbgdotbrush <rbg94@hotmail.it>
2024-01-30 13:43:51 +0100

    Refactor ControllableComponent using a WalkingComponent

--- app/src/main/java/com/personal/groucho/game/gameobjects/components/ControllableComponent.java
+++ app/src/main/java/com/personal/groucho/game/gameobjects/components/ControllableComponent.java
@@ -4,30 +4,25 @@ import static com.personal.groucho.game.Constants.characterDimensionsX;
 import static com.personal.groucho.game.Constants.characterDimensionsY;
 import static com.personal.groucho.game.Constants.maxLightIntensity;
 import static com.personal.groucho.game.Constants.minLightIntensity;
-import static com.personal.groucho.game.Constants.grouchoSpeed;
 import static com.personal.groucho.game.Utils.fromMetersToBufferX;
 import static com.personal.groucho.game.Utils.fromMetersToBufferY;
 import static com.personal.groucho.game.assets.Sounds.click;
 import static com.personal.groucho.game.assets.Sounds.loading;
+import static com.personal.groucho.game.assets.Spritesheets.groucho_walk;
 
 import com.personal.groucho.game.controller.ControllerObserver;
 import com.personal.groucho.game.GameWorld;
-import com.personal.groucho.game.controller.Orientation;
 import com.personal.groucho.game.assets.Sounds;
-import com.personal.groucho.game.Spritesheet;
 import com.personal.groucho.game.assets.Spritesheets;
 import com.personal.groucho.game.controller.Controller;
 import com.personal.groucho.game.controller.states.ControllerState;
 import com.personal.groucho.game.controller.states.NameState;
 
-public class ControllableComponent extends Component implements ControllerObserver {
+public class ControllableComponent extends WalkingComponent implements ControllerObserver {
 
     private final Controller controller;
     private final GameWorld gameworld;
-    private SpriteDrawableComponent spriteComponent = null;
-    private PhysicsComponent physicsComponent = null;
     private LightComponent lightComponent = null;
-    private PositionComponent positionComponent = null;
 
     private boolean playShotAnimation = false;
     private boolean playLoadingSound = true;
@@ -41,8 +36,6 @@ public class ControllableComponent extends Component implements ControllerObserv
     public ComponentType type() { return ComponentType.Controllable;}
 
     public void updatePlayerState() {
-        if (spriteComponent == null)
-            spriteComponent = (SpriteDrawableComponent) owner.getComponent(ComponentType.Drawable);
         if (lightComponent == null)
             lightComponent = (LightComponent) owner.getComponent(ComponentType.Light);
 
@@ -73,25 +66,7 @@ public class ControllableComponent extends Component implements ControllerObserv
 
     private void handleWalkingPlayer() {
         playLoadingSound = true;
-        if(physicsComponent == null)
-            physicsComponent = (PhysicsComponent) owner.getComponent(ComponentType.Physics);
-
-        updateSprite(Spritesheets.groucho_walk);
-
-        switch (positionComponent.getOrientation()) {
-            case UP:
-                updatePosition(0, grouchoSpeed *(-1));
-                break;
-            case DOWN:
-                updatePosition(0, grouchoSpeed *(1));
-                break;
-            case LEFT:
-                updatePosition(grouchoSpeed *(-1),0);
-                break;
-            case RIGHT:
-                updatePosition(grouchoSpeed *(1),0);
-                break;
-        }
+        walking(groucho_walk);
     }
 
     private void handleAimingPlayer() {
@@ -149,22 +124,8 @@ public class ControllableComponent extends Component implements ControllerObserv
         gameworld.shootEvent(originX, originY, endX, endY);
     }
 
-    private void updatePosition(float increaseX, float increaseY) {
-        physicsComponent.updatePosX(increaseX);
-        physicsComponent.updatePosY(increaseY);
-    }
-
-    private void updateSprite(Spritesheet sheet) {
-        spriteComponent.setCurrentSpritesheet(sheet);
-        spriteComponent.setAnimation(positionComponent.getOrientation().getValue());
-    }
-
     @Override
     public void update(ControllerState currentState) {
-        if (spriteComponent == null)
-            spriteComponent = (SpriteDrawableComponent) owner.getComponent(ComponentType.Drawable);
-        if (lightComponent == null)
-            lightComponent = (LightComponent) owner.getComponent(ComponentType.Light);
         if (positionComponent == null)
             positionComponent = (PositionComponent) owner.getComponent(ComponentType.Position);
 
---
+++ app/src/main/java/com/personal/groucho/game/gameobjects/components/WalkingComponent.java
@@ -0,0 +1,49 @@
+package com.personal.groucho.game.gameobjects.components;
+
+import static com.personal.groucho.game.Constants.grouchoSpeed;
+
+import com.personal.groucho.game.Spritesheet;
+
+public abstract class WalkingComponent extends Component {
+    protected PositionComponent positionComponent = null;
+    protected SpriteDrawableComponent spriteComponent = null;
+    protected PhysicsComponent physicsComponent = null;
+
+    protected void walking(Spritesheet sheet) {
+        if (positionComponent == null)
+            positionComponent = (PositionComponent) owner.getComponent(ComponentType.Position);
+
+        updateSprite(sheet);
+
+        switch (positionComponent.getOrientation()) {
+            case UP:
+                updatePosition(0, grouchoSpeed *(-1));
+                break;
+            case DOWN:
+                updatePosition(0, grouchoSpeed *(1));
+                break;
+            case LEFT:
+                updatePosition(grouchoSpeed *(-1),0);
+                break;
+            case RIGHT:
+                updatePosition(grouchoSpeed *(1),0);
+                break;
+        }
+    }
+
+    protected void updateSprite(Spritesheet sheet) {
+        if (spriteComponent == null)
+            spriteComponent = (SpriteDrawableComponent) owner.getComponent(ComponentType.Drawable);
+
+        spriteComponent.setCurrentSpritesheet(sheet);
+        spriteComponent.setAnimation(positionComponent.getOrientation().getValue());
+    }
+
+    private void updatePosition(float increaseX, float increaseY) {
+        if(physicsComponent == null)
+            physicsComponent = (PhysicsComponent) owner.getComponent(ComponentType.Physics);
+
+        physicsComponent.updatePosX(increaseX);
+        physicsComponent.updatePosY(increaseY);
+    }
+}
