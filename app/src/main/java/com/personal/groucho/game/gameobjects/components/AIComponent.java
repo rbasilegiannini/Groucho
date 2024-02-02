1ccbca438fa988e793ea92a4429726002a7c4baa
rbgdotbrush <rbg94@hotmail.it>
2024-02-02 16:15:31 +0100

    General fixes

--- app/src/main/java/com/personal/groucho/game/gameobjects/components/AIComponent.java
+++ app/src/main/java/com/personal/groucho/game/gameobjects/components/AIComponent.java
@@ -29,18 +29,17 @@ public class AIComponent extends WalkingComponent {
     private final GameWorld gameWorld;
     private final World world;
     private final AStar aStar;
-    private final Node positionOnGrid;
+    private Node positionOnGrid;
+    private Node playerPositionOnGrid;
     private List<Node> currentPath;
     private Node current;
     private boolean newNode = true;
-
 
     public AIComponent(GameWorld gameWorld, GameGrid grid) {
         this.gameWorld = gameWorld;
         this.world = gameWorld.getWorld();
         fsm = new FSM(new Idle(this));
         aStar = new AStar(grid);
-        positionOnGrid = new Node(0,0);
         currentPath = new ArrayList<>();
         currentSteps = 0;
         maxSteps = 1000;
@@ -92,18 +91,13 @@ public class AIComponent extends WalkingComponent {
     public void entryEngageAction(){
         if (positionComponent == null)
             positionComponent = (PositionComponent) owner.getComponent(ComponentType.Position);
-        positionOnGrid.setPosX(positionComponent.getPosX()/cellSize);
-        positionOnGrid.setPosY(positionComponent.getPosY()/cellSize);
 
-        Node playerPositionOnGrid = new Node(
-                (int) gameWorld.getPlayerPosition().getX()/cellSize,
-                (int) gameWorld.getPlayerPosition().getY()/cellSize
+        positionOnGrid = gameWorld.getGameGridNode(
+                positionComponent.getPositionXOnGrid(),
+                positionComponent.getPositionYOnGrid()
         );
 
-        currentPath.clear();
-        currentPath = aStar.findPath(positionOnGrid, playerPositionOnGrid);
-
-        // if (currentPath.isEmpty()) guard to comeback in patrol
+        setPathToPlayer();
     }
 
     public void activeEngageAction(){
@@ -112,6 +106,11 @@ public class AIComponent extends WalkingComponent {
                 current = currentPath.remove(0);
                 newNode = false;
             }
+
+            positionOnGrid = gameWorld.getGameGridNode(
+                    positionComponent.getPositionXOnGrid(),
+                    positionComponent.getPositionYOnGrid()
+            );
 
             if (positionOnGrid.posX != current.posX) {
                 walkingToXCoordinate(current.getPosX());
@@ -127,15 +126,24 @@ public class AIComponent extends WalkingComponent {
             spriteComponent.setCurrentSpritesheet(skeleton_idle);
     }
 
+    private void setPathToPlayer() {
+        playerPositionOnGrid = gameWorld.getGameGridNode(
+                (int) gameWorld.getPlayerPosition().getX()/cellSize,
+                (int) gameWorld.getPlayerPosition().getY()/cellSize
+        );
+
+        currentPath.clear();
+        currentPath = aStar.findPath(positionOnGrid, playerPositionOnGrid);
+    }
+
     private void walkingToXCoordinate(int targetPosX){
-        positionOnGrid.setPosX(positionComponent.getPosX()/cellSize);
         int positionOnGridX = positionOnGrid.getPosX();
 
         if (positionOnGridX != targetPosX) {
             if (positionOnGridX < targetPosX) {
                 positionComponent.setOrientation(Orientation.RIGHT);
             }
-            if (positionOnGridX >= targetPosX) {
+            if (positionOnGridX > targetPosX) {
                 positionComponent.setOrientation(Orientation.LEFT);
             }
             walking(skeleton_walk, skeletonSpeed);
@@ -145,14 +153,13 @@ public class AIComponent extends WalkingComponent {
     }
 
     private void walkingToYCoordinate(int targetPosY){
-        positionOnGrid.setPosY(positionComponent.getPosY()/cellSize);
         int positionOnGridY = positionOnGrid.getPosY();
 
         if (positionOnGridY != targetPosY) {
             if (positionOnGridY < targetPosY) {
                 positionComponent.setOrientation(Orientation.DOWN);
             }
-            if (positionOnGridY >= targetPosY) {
+            if (positionOnGridY > targetPosY) {
                 positionComponent.setOrientation(Orientation.UP);
             }
             walking(skeleton_walk, skeletonSpeed);
