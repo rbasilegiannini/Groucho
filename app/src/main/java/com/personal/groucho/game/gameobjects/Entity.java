5078d315dbb18454c9af991de2fa537b428f318f
rbgdotbrush <rbg94@hotmail.it>
2024-02-23 17:55:01 +0100

    Replace Map with SparseArray in Entity Class

--- app/src/main/java/com/personal/groucho/game/gameobjects/Entity.java
+++ app/src/main/java/com/personal/groucho/game/gameobjects/Entity.java
@@ -1,35 +1,30 @@
 package com.personal.groucho.game.gameobjects;
 
-import java.util.HashMap;
-import java.util.Map;
+import android.util.SparseArray;
 
 public class Entity {
-    protected final Map<ComponentType, Component> components;
-
-    public Entity() {
-        components = new HashMap<>();
-    }
+    protected final SparseArray<Component> components = new SparseArray<>();
 
     public Component getComponent(ComponentType type) {
-        return components.get(type);
+        return components.get(type.hashCode());
     }
 
     public void addComponent(Component component) {
         component.setOwner(this);
-        components.put(component.type(), component);
+        components.put(component.type().hashCode(), component);
     }
 
     public void removeComponent(ComponentType type) {
-        Component component = components.get(type);
-        if (component != null) {
+        Component component = components.get(type.hashCode());
+        if (component != null){
             component.delete();
-            components.remove(type);
+            components.remove(type.hashCode());
         }
     }
 
     public void delete() {
-        for (Map.Entry<ComponentType, Component> component : components.entrySet()) {
-            component.getValue().delete();
+        for (int i = 0; i < components.size(); i++){
+            components.valueAt(i).delete();
         }
         components.clear();
     }
