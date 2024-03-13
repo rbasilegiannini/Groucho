29147b41e93aca656a2bb76e9800428aac3cc11c
rbgdotbrush <rbg94@hotmail.it>
2024-03-13 23:02:20 +0100

    General fixes

--- app/src/main/java/com/personal/groucho/game/constants/System.java
+++ app/src/main/java/com/personal/groucho/game/constants/System.java
@@ -3,8 +3,8 @@ package com.personal.groucho.game.constants;
 public class System {
     public static boolean godMode = false;
     public static boolean debugMode = false;
-    public static boolean fpsCounter = true;
-    public static boolean memoryUsage = true;
+    public static boolean fpsCounter = false;
+    public static boolean memoryUsage = false;
     public static final float charDimX = 32;
     public static final float charDimY = 32;
     public static final int charScaleFactor = 5;
